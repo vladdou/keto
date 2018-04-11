7a4ec2f381a60f4a3a1ebb343453dbd5427720d9
arekkas <aeneas@ory.am>
2018-04-12 00:28:34 +0200

    unstaged

--- vendor/github.com/ory/ladon/audit_logger.go
+++
@@ -1,7 +0,0 @@
-package ladon
-
-// AuditLogger tracks denied and granted authorizations.
-type AuditLogger interface {
-	LogRejectedAccessRequest(request *Request, pool Policies, deciders Policies)
-	LogGrantedAccessRequest(request *Request, pool Policies, deciders Policies)
-}
--- vendor/github.com/ory/ladon/audit_logger_noop.go
+++
@@ -1,9 +0,0 @@
-package ladon
-
-// AuditLoggerNoOp is the default AuditLogger, that tracks nothing.
-type AuditLoggerNoOp struct{}
-
-func (*AuditLoggerNoOp) LogRejectedAccessRequest(r *Request, p Policies, d Policies) {}
-func (*AuditLoggerNoOp) LogGrantedAccessRequest(r *Request, p Policies, d Policies)  {}
-
-var DefaultAuditLogger = &AuditLoggerNoOp{}
